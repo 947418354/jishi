c8c6fa973d5fe4f3e6f3ea2fc675eed316174282
zouhui <wy63wy63wy63@163.com>
2022-06-07 18:35:17 +0800

    feat: 单向链表反转

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,3 +17,21 @@ function numQ(num) {
 }
 console.log('输入123.456789', numQ(123.456789))
 console.log('输入123456', numQ(123456))
+
+/**
+ * 单向链表反转
+ * 思路: while整个链表,提前申明好一些变量用于存储上一个节点,当前节点等,每遍历到一个节点就将其下一个节点先存进当前临时节点变量,
+ * 再将next指向上一个结点的变量,再将前一个节点变量指向当前节点,当前节点变量指向临时的当前节点变量
+ * 
+ */
+function reverse(lian) {
+  let cur = lian[0]
+  let prev = null
+  let lCur = null
+  while (cur) {
+    lCur = cur.next
+    cur.next = prev
+    prev = lCur
+    cur = lCur
+  }
+}
